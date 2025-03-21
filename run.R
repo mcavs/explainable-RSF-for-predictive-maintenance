86824ca2f794d0abe8d212f504df6a5886f3dc5f
Yiğitcan YARDIMCI <147248981+Yigitcanyardimci@users.noreply.github.com>
2025-03-21 14:07:44 +0300

    Update run.R

--- run.R
+++ run.R
@@ -13,9 +13,14 @@ split_data <- data_split(task = surv_task)
 
 models <- train_models(task = surv_task, split = split_data)
 
-lime_results <- survlime_explain(surv_task, split_data, models)
 
+lime_results <- survlime_explain(surv_task, split_data, models)
 plot(lime_results$cox_lime, type = "local_importance")
 plot(lime_results$rsf_lime, type = "local_importance")
-
 print(lime_results)
+
+
+shap_results <- survshap_explain(surv_task, split_data, models)
+plot(shap_results$cox_shap)
+plot(shap_results$rsf_shap)
+print(shap_results)
