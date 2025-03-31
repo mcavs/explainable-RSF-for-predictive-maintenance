5946598955ff68c9475fcf063ba6e677630a5b29
Yiğitcan YARDIMCI <147248981+Yigitcanyardimci@users.noreply.github.com>
2025-03-31 22:03:32 +0300

    Update survlime_explain.R

--- survlime_explain.R
+++ survlime_explain.R
@@ -1,74 +1,10 @@
 #################################################################
-# SURVLIME 
-#################################################################
-
-survlime_explain <- function(task, split, models) {
-  library(survex)
-  
-  cox_explainer <- explain(models$cox_model, 
-                           data = as.data.frame(task$data()), 
-                           y = task$truth(), 
-                           predict_function = predict)
-  
-  rsf_explainer <- explain(models$rsf_model, 
-                           data = as.data.frame(task$data()), 
-                           y = task$truth(), 
-                           predict_function = predict)
-  
-  observation <- as.data.frame(task$data())[5, , drop = FALSE]
-  
-  cox_lime <- predict_parts(cox_explainer, observation, type = "survlime")
-  rsf_lime <- predict_parts(rsf_explainer, observation, type = "survlime")
-  
-  plot(cox_lime, type = "local_importance")
-  plot(rsf_lime, type = "local_importance")
-  
-  return(list(
-    cox_lime = cox_lime,
-    rsf_lime = rsf_lime
-  ))
-}
-
-
-#################################################################
-# SURVSHAP
-#################################################################
-
-survshap_explain <- function(task, split, models) {
-  library(survex)
-
-  cox_explainer <- explain(models$cox_model, 
-                           data = as.data.frame(task$data()), 
-                           y = task$truth(), 
-                           predict_function = predict)
-
-  rsf_explainer <- explain(models$rsf_model, 
-                           data = as.data.frame(task$data()), 
-                           y = task$truth(), 
-                           predict_function = predict)
-
-  observation <- as.data.frame(task$data())[5, , drop = FALSE]
-
-  cox_shap <- predict_parts(cox_explainer, observation, type = "survshap")
-  rsf_shap <- predict_parts(rsf_explainer, observation, type = "survshap")
-
-  plot(cox_shap)
-  plot(rsf_shap)
-
-  return(list(
-    cox_shap = cox_shap,
-    rsf_shap = rsf_shap
-  ))
-}
-
-
-#################################################################
-# Global Açıklama
+# Global Açıklama (Variable Importance - Partial Dependence)
 #################################################################
 
 global_explain <- function(task, models) {
   library(survex)
-  
+
   cox_explainer <- explain(models$cox_model,
                            data = as.data.frame(task$data()),
                            y = task$truth(),
@@ -82,12 +18,13 @@ global_explain <- function(task, models) {
   cox_parts <- model_parts(cox_explainer)
   rsf_parts <- model_parts(rsf_explainer)
   
-  plot(cox_parts)
-  plot(rsf_parts)
+  cox_profile <- model_profile(cox_explainer)
+  rsf_profile <- model_profile(rsf_explainer)
   
   return(list(
-    cox = cox_parts,
-    rsf = rsf_parts
+    cox_parts   = cox_parts,
+    rsf_parts   = rsf_parts,
+    cox_profile = cox_profile,
+    rsf_profile = rsf_profile
   ))
 }
-
