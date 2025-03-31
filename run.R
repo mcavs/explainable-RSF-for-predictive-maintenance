ecb234f6a32aaa9fafd11d652b4f13862a30a054
Yiğitcan YARDIMCI <147248981+Yigitcanyardimci@users.noreply.github.com>
2025-03-31 22:04:18 +0300

    Update run.R

--- run.R
+++ run.R
@@ -6,27 +6,16 @@ source("modeling.R")
 source("performance.R")
 source("survlime_explain.R")
 
-
 surv_task <- make_task()
-
 split_data <- data_split(task = surv_task)
-
 models <- train_models(task = surv_task, split = split_data)
 
+global_results <- global_explain(surv_task, models)
 
-lime_results <- survlime_explain(surv_task, split_data, models)
-plot(lime_results$cox_lime, type = "local_importance")
-plot(lime_results$rsf_lime, type = "local_importance")
-print(lime_results)
+plot(global_results$cox_parts) + ggtitle("Variable Importance - Cox Model")
+plot(global_results$rsf_parts) + ggtitle("Variable Importance - RSF Model")
 
-
-shap_results <- survshap_explain(surv_task, split_data, models)
-plot(shap_results$cox_shap)
-plot(shap_results$rsf_shap)
-print(shap_results)
-
-
-global_results <- survshap_global(surv_task, split_data, models)
 print(global_results)
-plot(global_results$cox_global)
-plot(global_results$rsf_global)
+
+plot(global_results$cox_profile)
+plot(global_results$rsf_profile)
