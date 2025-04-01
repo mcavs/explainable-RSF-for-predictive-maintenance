9c2037d05e6a590c99939b81075b68362c385c19
Yiğitcan YARDIMCI <147248981+Yigitcanyardimci@users.noreply.github.com>
2025-04-02 01:24:16 +0300

    Update survlime_explain.R

--- survlime_explain.R
+++ survlime_explain.R
@@ -1,10 +1,6 @@
-#################################################################
-# Global Açıklama (Variable Importance - Partial Dependence)
-#################################################################
-
 global_explain <- function(task, models) {
   library(survex)
-
+  
   cox_explainer <- explain(models$cox_model,
                            data = as.data.frame(task$data()),
                            y = task$truth(),
@@ -18,13 +14,10 @@ global_explain <- function(task, models) {
   cox_parts <- model_parts(cox_explainer)
   rsf_parts <- model_parts(rsf_explainer)
   
-  cox_profile <- model_profile(cox_explainer)
-  rsf_profile <- model_profile(rsf_explainer)
-  
   return(list(
-    cox_parts   = cox_parts,
-    rsf_parts   = rsf_parts,
-    cox_profile = cox_profile,
-    rsf_profile = rsf_profile
+    cox_parts      = cox_parts,
+    rsf_parts      = rsf_parts,
+    cox_explainer  = cox_explainer,
+    rsf_explainer  = rsf_explainer
   ))
 }
