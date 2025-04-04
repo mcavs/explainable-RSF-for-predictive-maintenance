98bd4c6d54b5783031cf545f8db346f70b99734b
Yiğitcan YARDIMCI <147248981+Yigitcanyardimci@users.noreply.github.com>
2025-04-04 14:21:02 +0300

    Update run.R

--- run.R
+++ run.R
@@ -11,9 +11,10 @@ surv_task <- make_task()
 split_data <- data_split(task = surv_task)
 models <- train_models(task = surv_task, split = split_data)
 
-
 global_results <- global_explain(surv_task, models)
 
+
+# PDP 
 
 rsf_vars <- c("sensor_12", "sensor_15", "sensor_4", 
               "sensor_21", "sensor_14", "sensor_3", "sensor_7")
@@ -24,10 +25,13 @@ cox_vars <- c("sensor_14", "sensor_4", "sensor_8",
 rsf_profile_selected <- model_profile(global_results$rsf_explainer, variables = rsf_vars)
 cox_profile_selected <- model_profile(global_results$cox_explainer, variables = cox_vars)
 
-plot(rsf_profile_selected)
-plot(cox_profile_selected) 
+plot(rsf_profile_selected) +
+  xlim(c(125, 250))
+plot(cox_profile_selected) +
+  xlim(c(125, 250))
 
 
+# 
 rsf_vimp <- plot(global_results$rsf_parts) +
   xlim(c(125, 250)) +
   theme(legend.title = element_blank(),
@@ -45,3 +49,12 @@ cox_vimp <- plot(global_results$cox_parts) +
         legend.position = "bottom") 
 
 plot_grid(rsf_vimp, cox_vimp, labels = c("A", "B"))
+
+
+
+perf_results <- measure_performance(models, surv_task, split_data)
+perf_results$c_index
+perf_results$brier
+perf_results$auc
+
+
